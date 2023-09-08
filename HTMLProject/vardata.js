db40591fb357a1b00c867deec02aa67f6d047664
Csilla Bukki <csillabukki@Csillas-Air.hsd1.ca.comcast.net>
2023-09-08 13:30:58 -0700

    JavaScript Variables and Data Types

---
+++ HTMLProject/vardata.js
@@ -0,0 +1,12 @@
+let name = "Csilla Bukki";
+let dayOfBirth = 5;
+//Uncaught SyntaxError: Identifier 'name' has already been declared
+//let name = "Lua Montana";
+let color = prompt("Please type your favorite color. \n");
+console.log(`Hello there, your favorite color is ${color}.`);
+const str = "I live in the Bay Area. It's my favorite hiking place.";
+//null: The value of the variable is set to nothing intentionally. The variable is already decleared before.
+//undefined: It also means that the value of the variable is nothing, but it happens when the variable is decleared.
+//It is not intentionally happens.
+//NaN means Not a Number. For example 0/0 gives NaN or Number("hello"). The typeof NaN is 'number'.
+//Creating a 'let thing' variable results in the value of the variable thing being undefined.

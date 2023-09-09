90bebe8493965eba2c43fc183e2e319755dac716
Csilla Bukki <csillabukki@Csillas-Air.hsd1.ca.comcast.net>
2023-09-08 18:02:59 -0700

    JavaScript Loops

---
+++ HTMLProject/loops.js
@@ -0,0 +1,21 @@
+for (let i = 0; i < 11; i++) {
+  console.log(i);
+}
+
+let j = 0;
+while (j < 11) {
+  console.log(j);
+  j++;
+}
+
+for (let i = 0; i <= 20; i++) {
+  if (i % 2 === 0) console.log(i);
+}
+let k = 1;
+while (k < 20) {
+  if (k % 2 === 1) console.log(k);
+  k++;
+}
+for (let b = 10; b >= 0; b--) {
+  console.log(b);
+}

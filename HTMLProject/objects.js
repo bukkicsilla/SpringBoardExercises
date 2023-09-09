b1b3d4759c66a32af764eeff87d996a4926ba8d1
Csilla Bukki <csillabukki@Csillas-Air.hsd1.ca.comcast.net>
2023-09-08 18:02:03 -0700

    JavaScript Objects

---
+++ HTMLProject/objects.js
@@ -0,0 +1,11 @@
+let programming = {
+  languages: ["JavaScript", "Python", "Ruby"],
+  isChallenging: true,
+  isRewarding: true,
+  difficulty: 8,
+  jokes: "https://bit.ly/2ysFran",
+};
+programming.languages.push("Go"); //step 1.
+programming.difficulty = 7; //step 2.
+delete programming.jokes; //step 3.
+programming["isFun"] = true; //step 4.
